93632014ac7473a4cf01897c156e00438a2fe398
Alejandro Curci <76498083+acurci-at-mulesoft@users.noreply.github.com>
2021-04-21 12:10:02 -0300

    Libraries

--- src/com/mule/support/handlers/SupportTicketDialog.java
+++ src/com/mule/support/handlers/SupportTicketDialog.java
@@ -18,6 +18,7 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.json.JSONException;
 
 
 public class SupportTicketDialog extends TitleAreaDialog {
@@ -186,10 +187,9 @@ public class SupportTicketDialog extends TitleAreaDialog {
         if(btnIncludeLogs.getSelection()){
         	attachments.addAll(CollectFiles.getLog(project));     	
         }
-
         String issueNumber = SalesforceServices.createSupportCase(projectName, message, attachments);
         
-        
+        System.out.println(issueNumber);
         
 
     }
